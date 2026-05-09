4e310ca75b60e2c535f4cf5974fdaf95bbbfca3d
hyeonpark-5 <sorkgus@gmail.com>
2026-05-09 21:34:06 +0900

    feat: 회원가입 화면 이메일 입력칸 추가

--- app/src/main/java/com/example/thirdpj/ui/auth/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/thirdpj/ui/auth/signup/SignUpScreen.kt
@@ -1,25 +1,37 @@
 package com.example.thirdpj.ui.auth.signup
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.thirdpj.ui.theme.ThirdPJTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignUpScreen() {
+    var email by remember { mutableStateOf("") }
     // 상단바, Scaffold
     Scaffold(
         // 화면 상단바를 만들기 위해서 Scaffold의 Topbar 사용
@@ -42,8 +54,24 @@ fun SignUpScreen() {
             )
         }
     ) {innerPadding ->
-        Box(modifier = Modifier.padding(innerPadding)){
-            Text("상단바 틀 잡기")
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+           OutlinedTextField(
+               value = email,
+               onValueChange = {email = it},
+               label = {Text("이메일")},
+               placeholder = {Text("example@gmail.com")},
+               modifier = Modifier.fillMaxWidth(),
+               keyboardOptions = KeyboardOptions(
+                   keyboardType = KeyboardType.Email,
+                   imeAction = ImeAction.Next
+               )
+
+           )
         }
     }
 }
