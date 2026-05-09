5e93c220976ef7f57743907ca2b042823d505709
hyeonpark-5 <sorkgus@gmail.com>
2026-05-10 00:01:16 +0900

    feat: 로그인 화면 회원가입 textbutton 구현

--- app/src/main/java/com/example/thirdpj/ui/auth/login/LoginScreen.kt
+++ app/src/main/java/com/example/thirdpj/ui/auth/login/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.example.thirdpj.ui.auth.login
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -19,6 +21,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -60,6 +63,33 @@ fun LoginScreen() {
                 ) {
                     Text("이메일로 로그인하기")
                 }
+
+                // 회원가입 이동
+                HorizontalDivider(
+                    modifier = Modifier.padding(vertical = 24.dp),
+                    thickness = 1.dp,
+                    color = Color.LightGray
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "아직 계정이 없으신가요?",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.Gray
+                    )
+                    TextButton(
+                        onClick = {}
+                    ) {
+                        Text(
+                            text = "회원가입",
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+
+
             } else {
                 Text(
                     text = "이메일로 로그인",
@@ -108,6 +138,8 @@ fun LoginScreen() {
 
             }
         }
+
+
 
     }
 }
