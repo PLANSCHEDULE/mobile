669ac1213f91705b36142a460c4ccc09021e5e0f
hyeonpark-5 <sorkgus@gmail.com>
2026-05-10 14:31:24 +0900

    feat: HomeScreen 기본 틀 잡기

---
+++ app/src/main/java/com/example/thirdpj/ui/home/screen/HomeScreen.kt
@@ -0,0 +1,33 @@
+package com.example.thirdpj.ui.home.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.thirdpj.ui.home.components.HomeHeader
+import com.example.thirdpj.ui.theme.ThirdPJTheme
+
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            HomeHeader()
+        },
+        containerColor = Color.White
+    ) {innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    ThirdPJTheme {
+        HomeScreen()
+    }
+}
