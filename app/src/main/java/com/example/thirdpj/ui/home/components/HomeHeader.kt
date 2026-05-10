293cbea12a4a1739b39fa12e616bc9b205df47cb
hyeonpark-5 <sorkgus@gmail.com>
2026-05-10 14:08:41 +0900

    feat: HomeHeader 검색 아이콘 추가

--- app/src/main/java/com/example/thirdpj/ui/home/components/HomeHeader.kt
+++ app/src/main/java/com/example/thirdpj/ui/home/components/HomeHeader.kt
@@ -4,6 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.thirdpj.ui.theme.ThirdPJTheme
 
 @Composable
@@ -19,15 +25,28 @@ fun HomeHeader() {
     // 파일 구조를 어떻게 잡아야되지
     Column() {
         Row(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            // 이게 있어야 검색 아이콘과 텍스트간의 공간이 생김
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
                 text = "홍길동",
                 style = MaterialTheme.typography.headlineMedium,
                 fontWeight = FontWeight.ExtraBold,
                 color = Color.Black
-
             )
+
+            // 검색 아이콘
+            IconButton(
+                onClick = {}
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "검색",
+                    modifier = Modifier.size(28.dp)
+                )
+            }
         }
     }
 
