3b361b2b0d92606d0a7395faa513f6dba22aa715
hyeonpark-5 <sorkgus@gmail.com>
2026-05-10 19:49:59 +0900

    feat: HomeScreen에 HomeBanner 추가

--- app/src/main/java/com/example/thirdpj/ui/home/screens/HomeScreen.kt
+++ app/src/main/java/com/example/thirdpj/ui/home/screens/HomeScreen.kt
@@ -1,13 +1,18 @@
 package com.example.thirdpj.ui.home.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.thirdpj.ui.components.BottomBar
+import com.example.thirdpj.ui.home.components.HomeBanner
 import com.example.thirdpj.ui.home.components.HomeHeader
 import com.example.thirdpj.ui.theme.ThirdPJTheme
 
@@ -24,7 +29,13 @@ fun HomeScreen() {
 
         containerColor = Color.White
     ) {innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding)) {
+        Column(modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize()
+            .padding(horizontal = 15.dp)
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            HomeBanner()
 
         }
     }
