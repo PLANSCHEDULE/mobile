306a165026e8a4715e1cd9ea936bbc7cb6832150
hyeonpark-5 <sorkgus@gmail.com>
2026-05-11 11:57:42 +0900

    feat: 일정 생성 screen 틀 구현

---
+++ app/src/main/java/com/example/thirdpj/ui/create/screens/CreatePlanScreen.kt
@@ -0,0 +1,20 @@
+package com.example.thirdpj.ui.create.screens
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.thirdpj.ui.theme.ThirdPJTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CreatePlanScreen() {
+
+}
+
+@Preview
+@Composable
+fun CreatePlanScreenPreview() {
+    ThirdPJTheme {
+        CreatePlanScreen()
+    }
+}
