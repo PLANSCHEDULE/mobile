9d171426aa58d7c163e2da63efed9b3b2b16af90
hyeonpark-5 <sorkgus@gmail.com>
2026-05-11 18:43:01 +0900

    feat: HeartTemplateCard 설명 텍스트 추가

--- app/src/main/java/com/example/thirdpj/ui/profile/components/HeartTemplateCard.kt
+++ app/src/main/java/com/example/thirdpj/ui/profile/components/HeartTemplateCard.kt
@@ -3,9 +3,11 @@ package com.example.thirdpj.ui.profile.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -13,14 +15,20 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.thirdpj.R
 import com.example.thirdpj.ui.theme.ThirdPJTheme
 
@@ -60,6 +68,35 @@ fun HeartTemplateCard(
             }
 
             Spacer(modifier = Modifier.width(16.dp))
+
+            // 중간 텍스트 2 영역이니까 Column? Row는 아닌 듯..
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    // 그냥 row로 하면 뭔가 애매한데
+                    // 찾아보니 buildAnnotatedString 사용하면 줄 정렬, 바꿈이 더 자연스럽게 되는게 있음, text = 에 넣어서 사용하면 되는 듯
+                    text = buildAnnotatedString {
+                        append("하트 누른 템플릿 ")
+                        //오 이렇게 적용하니까 디자인이랑 거의 비슷하게 됨
+                        withStyle(style = SpanStyle(color = Color.Red, fontWeight = FontWeight.Bold)) {
+                            append("32")
+                        }
+                    },
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                // 설명
+                Text(
+                    text = "찜한 일정 모아보기",
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Gray
+                )
+            }
         }
     }
 }
