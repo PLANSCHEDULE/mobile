27b4c17abc4759c317464f93c85ec4c3ba83248d
hyeonpark-5 <sorkgus@gmail.com>
2026-05-11 14:13:48 +0900

    feat: plan 시간 영역 구현

--- app/src/main/java/com/example/thirdpj/ui/plan/components/Plan.kt
+++ app/src/main/java/com/example/thirdpj/ui/plan/components/Plan.kt
@@ -1,16 +1,22 @@
 package com.example.thirdpj.ui.plan.components
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.thirdpj.R
@@ -18,12 +24,15 @@ import com.example.thirdpj.ui.data.PlanItem
 import com.example.thirdpj.ui.theme.ThirdPJTheme
 
 @Composable
-fun Plan(item: PlanItem, onCheckedChange: (Boolean) -> Unit) {
+fun Plan(item: PlanItem,
+         onCheckedChange: (Boolean) -> Unit,
+         onTimeClick: () -> Unit
+) {
     Surface(
         modifier = Modifier.Companion
             .fillMaxWidth()
             .padding(vertical = 6.dp),
-        color = Color.Companion.White,
+        color = Color.White,
         shape = RoundedCornerShape(8.dp),
         shadowElevation = 2.dp
     ) {
@@ -37,8 +46,28 @@ fun Plan(item: PlanItem, onCheckedChange: (Boolean) -> Unit) {
             Icon(
                 painter = painterResource(id = R.drawable.drag_indicator_icon),
                 contentDescription = null,
-                tint = Color.Companion.Gray
+                tint = Color.Companion.Gray,
+                modifier = Modifier.size(24.dp)
             )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            // 시간
+            Surface(
+                onClick = onTimeClick,
+                color = Color.Transparent,
+                shape = RoundedCornerShape(4.dp)
+            ) {
+                // 시간 선택을 어떻게 하면 좋을까, 선택? 시간 선택 컴포넌트를 따로 만들어야 하는 듯
+                Text(
+                    text = item.time,
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = if(item.isDone) Color.LightGray else Color(0xFFF27A54)
+                    ),
+                    modifier = Modifier.padding(horizontal = 4.dp, vertical = 6.dp)
+                )
+            }
         }
 
         // 삭제는 어떻게 구현해야 하나? 옆에 쓰레기통 아이콘 표시?
@@ -59,7 +88,8 @@ fun PlanPreview(
         )
         Plan(
             item = dummyData,
-            onCheckedChange = {}
+            onCheckedChange = {},
+            onTimeClick = {}
         )
     }
 }
