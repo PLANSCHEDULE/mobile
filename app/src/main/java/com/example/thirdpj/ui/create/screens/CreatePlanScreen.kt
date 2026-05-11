9ad9f5ac43205ddbd629fad5314e5cce97c6480f
hyeonpark-5 <sorkgus@gmail.com>
2026-05-11 12:01:27 +0900

    feat: 일정 생성 screen topbar, header 추가

--- app/src/main/java/com/example/thirdpj/ui/create/screens/CreatePlanScreen.kt
+++ app/src/main/java/com/example/thirdpj/ui/create/screens/CreatePlanScreen.kt
@@ -1,13 +1,34 @@
 package com.example.thirdpj.ui.create.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.thirdpj.ui.create.components.CreatePlanHeader
+import com.example.thirdpj.ui.create.components.CreatePlanTopBar
 import com.example.thirdpj.ui.theme.ThirdPJTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreatePlanScreen() {
+    Scaffold(
+        topBar = {
+            CreatePlanTopBar()
+        }
+
+    ) {innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+        ) {
+            CreatePlanHeader()
+        }
+
+    }
 
 }
 
