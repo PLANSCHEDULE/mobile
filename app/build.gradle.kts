0783f1d7a89464a92499c6601679dbe5c8a33f35
hyeonpark-5 <sorkgus@gmail.com>
2026-05-12 20:01:48 +0900

    chore: retrofit 관련 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.9")
     // ViewModel & Lifecycle
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
