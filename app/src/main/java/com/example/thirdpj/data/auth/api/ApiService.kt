2d4ac6d845ce4bbab30652b0ee1a0be5142b246e
hyeonpark-5 <sorkgus@gmail.com>
2026-05-13 09:43:54 +0900

    feat: 공통 응답 대응 dto 구현

---
+++ app/src/main/java/com/example/thirdpj/data/auth/api/ApiService.kt
@@ -0,0 +1,10 @@
+package com.example.thirdpj.data.auth.api
+
+import com.example.thirdpj.data.auth.dto.SignUpRequest
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+    @POST("/api/auth/signup")
+    suspend fun signUp(@Body request: SignUpRequest)
+}
---
+++ app/src/main/java/com/example/thirdpj/data/auth/network/RetrofitClient.kt
@@ -0,0 +1,7 @@
+package com.example.thirdpj.data.auth.network
+
+// 통신을 담당하는 역할
+object RetrofitClient {
+    private const val BASE_URL = "http://10.0.2.2:8080"
+
+}
