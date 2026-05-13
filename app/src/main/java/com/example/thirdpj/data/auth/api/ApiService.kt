9dc73bfdc2476eaafc8ced5908644853c2bdd024
hyeonpark-5 <sorkgus@gmail.com>
2026-05-13 09:46:13 +0900

    feat: 회원가입 response 설정

--- app/src/main/java/com/example/thirdpj/data/auth/api/ApiService.kt
+++ app/src/main/java/com/example/thirdpj/data/auth/api/ApiService.kt
@@ -1,10 +1,13 @@
 package com.example.thirdpj.data.auth.api
 
 import com.example.thirdpj.data.auth.dto.SignUpRequest
+import com.example.thirdpj.data.auth.dto.SignUpResponse
+import com.example.thirdpj.data.dto.ApiResponse
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ApiService {
     @POST("/api/auth/signup")
-    suspend fun signUp(@Body request: SignUpRequest)
+    suspend fun signUp(@Body request: SignUpRequest): Response<ApiResponse<SignUpResponse>>
 }
