ce48652537ff8115926a093f9ced6051af3a1f42
hyeonpark-5 <sorkgus@gmail.com>
2026-05-13 17:35:39 +0900

    feat: profileScreen 폼 추가

--- app/src/main/java/com/example/thirdpj/ui/profile/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/thirdpj/ui/profile/screens/ProfileScreen.kt
@@ -1,7 +1,12 @@
 package com.example.thirdpj.ui.profile.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -10,7 +15,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.thirdpj.data.profile.dto.ProfileResponse
+import com.example.thirdpj.ui.profile.components.ProfileForm
 import com.example.thirdpj.ui.profile.components.ProfileTopBar
 import com.example.thirdpj.ui.theme.ThirdPJTheme
 
@@ -47,12 +54,23 @@ fun ProfileScreen(
     ) {innerPadding ->
         Column(
             modifier = Modifier.padding(innerPadding)
+                .fillMaxSize()
+                .padding(horizontal = 20.dp)
+                .verticalScroll(rememberScrollState())
         ) {
-
+            ProfileForm(
+                handle = handle,
+                onHandleChange = {if(it.length <= 50) handle = it},
+                isHandleEditable = !isEditMode,
+                nickname = nickname,
+                onNicknameChange = {if(it.length <= 20) nickname = it},
+                bio = bio,
+                onBioChange = {if(it.length <=150) bio = it}
+            )
+                Spacer(modifier = Modifier.height(40.dp))
         }
 
     }
-
 
 }
 
@@ -70,8 +88,8 @@ fun ProfileEditPreview() {
     ThirdPJTheme {
         ProfileScreen(
             initialProfile = ProfileResponse(
-                handle = "gemini_user",
-                nickname = "제미니",
+                handle = "gildong",
+                nickname = "홍길동",
                 bio = "안녕하세요! 안드로이드 개발 중입니다."
             )
         )
