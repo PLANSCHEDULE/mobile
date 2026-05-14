a54f0cc1a014eb24be451b19a89c682e345f4e44
hyeonpark-5 <sorkgus@gmail.com>
2026-05-14 15:30:09 +0900

    feat: DetailHeader 뒤로가기 버튼 구현

---
+++ app/src/main/java/com/example/thirdpj/ui/detail/components/DetailHeader.kt
@@ -0,0 +1,69 @@
+package com.example.thirdpj.ui.detail.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.thirdpj.R
+
+@Composable
+fun DetailHeader(
+    onBackClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(280.dp)
+            .background(Color(0xFFFE964A))
+    ) {
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black.copy(alpha = 0.2f)
+                        )
+                    )
+
+                )
+        )
+
+        // 뒤로 가기 버튼
+        IconButton(
+            onClick = onBackClick,
+            modifier = Modifier
+                .padding(top = 16.dp, start = 8.dp)
+                .align(Alignment.TopStart)
+        ) {
+            Icon(painter = painterResource(id = R.drawable.arrow_back),
+                contentDescription = "뒤로가기",
+                tint = Color.White)
+        }
+
+    }
+
+}
+
+@Preview
+@Composable
+fun DetailHeaderPreview() {
+    DetailHeader(
+        onBackClick = {}
+
+    )
+}
