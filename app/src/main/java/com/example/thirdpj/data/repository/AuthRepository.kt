5aa7e2e8c783de095e988ced45cffa2c55fe1728
hyeonpark-5 <sorkgus@gmail.com>
2026-05-14 19:24:29 +0900

    feat: AuthRepository 구현

--- app/src/main/java/com/example/thirdpj/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/thirdpj/data/repository/AuthRepository.kt
@@ -1,4 +1,11 @@
 package com.example.thirdpj.data.repository
 
-class AuthRepository() {
+import com.example.thirdpj.data.api.AuthService
+import com.example.thirdpj.data.auth.dto.LoginRequest
+import com.example.thirdpj.data.auth.dto.SignUpRequest
+
+class AuthRepository(private val authService: AuthService) {
+    suspend fun login(loginRequest: LoginRequest) = authService.login(loginRequest)
+
+    suspend fun signUp(signUpRequest: SignUpRequest) = authService.signUp(signUpRequest)
 }
