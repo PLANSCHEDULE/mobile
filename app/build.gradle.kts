e849820a49785ce09e85f5a17c36679c9ae1ccb5
hyeonpark-5 <sorkgus@gmail.com>
2026-05-15 10:30:36 +0900

    chore: dataStore 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,6 @@ dependencies {
     // retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //data store
+    implementation("androidx.datastore:datastore-preferences:1.2.1")
 }
